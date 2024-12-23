313b50d8b479129c1d2323fb7c2976d4c0386a03
marwan2232004 <118024824+marwan2232004@users.noreply.github.com>
2024-12-23 22:07:22 +0200

    feat: notebooks

--- inference/inference.py
+++ inference/inference.py
@@ -1,6 +1,8 @@
 import cv2
 import joblib
 import numpy as np
+import pytesseract
+from ultralytics import YOLO
 
 from utils.image_preprocessing import preprocess_image, find_plate_chars, get_chars_from_contours
 
@@ -40,5 +42,31 @@ def get_car_plate_characters(image_path):
     for char in chars:
         features = np.array(extract_features([char])).reshape(-1, 1).flatten()
         predictions += model.predict([features])[0]
+
+    return predictions
+
+
+def get_car_plate_characters_yolo(image_path):
+    best = YOLO(model="models/yolo_fine_tuned.pt")
+    results = best.predict(conf=0.25, source=image_path)
+    image = cv2.imread(image_path)
+    predictions = []
+    for result in results:
+        for box in result.boxes.xyxy:
+            x1, y1, x2, y2 = map(int, box)  # Convert coordinates to integers
+
+            # Crop the image using the bounding box
+            cropped_img = image[y1:y2, x1:x2]
+
+            # Ensure the cropped image is C-contiguous
+            cropped_img = np.ascontiguousarray(cropped_img)
+
+            # Save the cropped image temporarily
+            temp_path = f"./car_plate"
+            cv2.imwrite(temp_path, cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR))
+
+            pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+            text = pytesseract.image_to_string(cropped_img, config='--psm 7')
+            predictions.append(text.strip())
 
     return predictions
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 import cv2
 
-from inference.inference import get_car_plate_characters
+from inference.inference import get_car_plate_characters, get_car_plate_characters_yolo
 from utils.utils import show_images
 
 IMAGES_DIR = os.path.join("data", "car-plate-detection", "images")
@@ -11,7 +11,7 @@ IMAGE_WIDTH = 300
 IMAGE_HEIGHT = 200
 
 if __name__ == "__main__":
-    sample_image_name = "Cars0.png"
+    sample_image_name = "Cars96.png"
     image_path = os.path.join(IMAGES_DIR, sample_image_name)
 
     image = cv2.imread(image_path)
@@ -20,3 +20,7 @@ if __name__ == "__main__":
     predictions = get_car_plate_characters(image_path=image_path)
     # display the image
     show_images([original_image], [f"Car Plate Number: {predictions}"])
+
+    yolo_predications = get_car_plate_characters_yolo(image_path=image_path)
+    # display the image
+    show_images([original_image], [f"Car Plate Number: {yolo_predications}"])
